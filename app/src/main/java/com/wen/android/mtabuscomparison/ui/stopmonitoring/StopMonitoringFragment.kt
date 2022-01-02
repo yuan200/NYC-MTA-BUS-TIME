b3034f0654dfcd10a37623e1e8a241f5e4c61410
yuan <wenyuan0201@gmail.com>
2022-01-01 22:15:43 -0500

    update dependency

--- app/src/main/java/com/wen/android/mtabuscomparison/ui/stopmonitoring/StopMonitoringFragment.kt
+++ app/src/main/java/com/wen/android/mtabuscomparison/ui/stopmonitoring/StopMonitoringFragment.kt
@@ -264,7 +264,7 @@ class StopMonitoringFragment : Fragment(),
                                         )
                                     )
                                     .title(mViewModel.targetPublishedLineName.value)
-                            )
+                            )!!
                         )
                     }
                 }
